95228f27dadc4b0336e0fea9de5f91ba5c65c677
Reem Ibrahim Desouky Abu Al Safa <126808249+reemdesouky@users.noreply.github.com>
2025-11-17 14:00:45 +0200

    Added: getChildren fn

--- environment.py
+++ environment.py
@@ -12,3 +12,25 @@ class Connect4 ():
     def print_board(self):
         print(self.mat)
 
+    def addPeice(self, type, row, col):
+        if self.mat[row][col] == 0:
+            if type == RED:
+                self.mat[row][col] = 1
+            else :
+                self.mat[row][col] = -1
+        else :
+            print("Error: Slot is not empty!")
+
+    def getChildren(self, type):
+        children = []
+        for i in range (self.cols):
+            for j in range(self.rows-1 , -1 , -1):
+                if self.mat[j][i] == 0:
+                    child = self.mat.copy()
+                    if type == RED:
+                        child[j][i] = 1
+                    else:
+                        child[j][i] = -1
+                    children.append(child)
+                    break
+        return children
